01aa52edb5849691d64bd14b9097cd4db3f81e47
out-shashinov1-vs <satndupic87@gmail.com>
2021-09-27 17:09:10 +0300

    my first try in redux-toolkit

--- reducer/user/index.ts
+++ reducer/user/index.ts
@@ -1,17 +1,21 @@
-import {GET_USER} from "../../constants";
+import {ERROR_AUTH, GET_USER, SUCCESS_AUTH} from "../../constants";
 import {IUserAPIUserData} from "../../api/types/user";
 import {IssuerPointAPI} from "../../api/types/issuers";
+import {createReducer, createAction, createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import HttpClient from "../../api";
 
 interface IInitialState {
     currentUser: IUserAPIUserData | null
     hasToken: boolean,
     issuers:  IssuerPointAPI[] | undefined,
+    currentIssuer: IssuerPointAPI | undefined
 }
 
 const INITIAL_STATE: IInitialState = {
     currentUser: null,
     hasToken: false,
     issuers: undefined,
+    currentIssuer: undefined
 }
 
 type ACTION_TYPE = 
@@ -20,20 +24,41 @@ type ACTION_TYPE =
         payload: IUserAPIUserData
        }
 
-const User = (state = INITIAL_STATE, action: ACTION_TYPE) => {
-    switch (action.type){
-        case GET_USER:
-            const {issuers} = action.payload
-            return {
-                ...state,
-                currentUser: action.payload,
-                hasToken: true,
-                issuers: issuers,
-            }
-        default:
-            return state
 
+const fetchUser = createAsyncThunk(
+    'users/fetch',
+    async (_, thunkAPI) => {
+        const user = await HttpClient.get<IUserAPIUserData>('/users/me')
+        return user.data
     }
-}
+)       
+       
+const get_user = createAction<IUserAPIUserData>('user/get_user')
+       
+const User = createSlice({
+    name: 'user',
+    initialState: INITIAL_STATE,
+    reducers: {},
+    extraReducers: {
+        [`${fetchUser.fulfilled}`]: (state, action) => {
+            state.currentUser = action.payload;
+        }
+    }
+})
+// const User = (state = INITIAL_STATE, action: ACTION_TYPE) => {
+//     switch (action.type){
+//         case GET_USER:
+//             const {issuers} = action.payload
+//             return {
+//                 ...state,
+//                 currentUser: action.payload,
+//                 hasToken: true,
+//                 issuers: issuers,
+//             }
+//         default:
+//             return state
+//
+//     }
+// }
 
 export default User
--- store.ts
+++ store.ts
@@ -1,8 +1,13 @@
 import { createStore, applyMiddleware} from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
+import { configureStore } from '@reduxjs/toolkit';
 import reducers from './reducer';
-const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
+
+const store = configureStore({
+    reducer: reducers,
+})
+
 export default store;
 
 export type RootState = ReturnType<typeof store.getState>
